74564155de2b852644611bf0577a3df67628211c
Lucas Boa Nova <lucasbn2009@hotmail.com>
2022-07-14 19:59:54 -0300

    Database

--- backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -1,4 +1,4 @@
-package ocm.devsuperior.dsmeta;
+package com.devsuperior.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
